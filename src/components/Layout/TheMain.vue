56a9725ba2e254ba6cce0da3a41ffec569386d5f
nabil-y <nabil.yassine.dev@gmail.com>
2022-09-27 22:50:05 +0200

    refactor home page

---
+++ src/components/Layout/TheMain.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import HeroBanner from "@/components/Layout/HeroBanner.vue";
+import TrustClient from "@/components/Layout/TrustClient.vue";
+import FeatureCarousel from "@/components/Layout/FeatureCarousel.vue";
+import ValueSection from "@/components/Layout/ValueSection.vue";
+import ReviewSection from "@/components/Layout/ReviewSection.vue";
+import ContactUs from "@/components/Layout/ContactUs.vue";
+</script>
+
+<template>
+  <main id="#skip-nav">
+    <HeroBanner />
+    <div class="main-container">
+      <TrustClient />
+      <FeatureCarousel />
+      <ValueSection />
+      <ReviewSection />
+      <ContactUs />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.main-container {
+  max-width: 1121px;
+  margin: 0 auto;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,37 +1,16 @@
 <script setup lang="ts">
 import TheHeader from "@/components/Layout/TheHeader.vue";
-import HeroBanner from "@/components/Layout/HeroBanner.vue";
-import TrustClient from "@/components/Layout/TrustClient.vue";
-import FeatureCarousel from "@/components/Layout/FeatureCarousel.vue";
-import ValueSection from "@/components/Layout/ValueSection.vue";
-import ReviewSection from "@/components/Layout/ReviewSection.vue";
-import ContactUs from "@/components/Layout/ContactUs.vue";
+import TheMain from "../components/Layout/TheMain.vue";
 import TheFooter from "../components/Layout/TheFooter.vue";
 import AppModal from "../components/UI/AppModal.vue";
-import { useModalStore } from "@/stores/store";
 
+import { useModalStore } from "@/stores/store";
 const store = useModalStore();
 </script>
 
 <template>
   <TheHeader />
-  <main id="#skip-nav">
-    <HeroBanner />
-    <div class="main-container">
-      <TrustClient />
-      <FeatureCarousel />
-      <ValueSection />
-      <ReviewSection />
-      <ContactUs />
-    </div>
-  </main>
+  <TheMain />
   <TheFooter />
   <AppModal v-if="store.isModalVisible" />
 </template>
-
-<style scoped>
-.main-container {
-  max-width: 1121px;
-  margin: 0 auto;
-}
-</style>
